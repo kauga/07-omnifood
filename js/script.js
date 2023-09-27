860f6587465899393f2a47d2e3c2e8867b04d859
kauga <120273457+kauga@users.noreply.github.com>
2023-09-27 15:05:35 +0300

    new and end

--- js/script.js
+++ js/script.js
@@ -30,14 +30,44 @@ if (href === "#") window.scrollTo({
  });
 
  //stick navigation
+//  const sectionHeroEL = document.querySelector(".section-hero");
+//  const removalSectionHeroEL = document.querySelector(".section-hero");
+
+//  const obs = new IntersectionObserver(function(entries) {
+//   const ent = entries[0];
+//   if (ent.isIntersecting === false)
+//   document.body.classList.add("sticky")
+//  } else {
+// //The footer is not in view, add the "sticky" class
+//   document.body.classList.add("sticky")
+//   }, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: "-80px",
+//  })
+//  obs.observe(sectionHeroEL);
+// stickyObserver.observe(sectionHeroEL);
+// stickyObserver.observe(removalSectionHeroEL); 
+ //sticky mine
+
  const sectionHeroEL = document.querySelector(".section-hero");
- const obs = new IntersectionObserver(function(entries) {
-  const ent = entries[0];
-  if (ent.isIntersecting === false)
-  document.body.classList.add("sticky");
- }, {
+const footerEL = document.querySelector(".section-hero"); // Replace ".footer" with the actual selector for your footer element
+
+const stickyObserver = new IntersectionObserver(function(entries) {
+  const entry = entries[0];
+  
+  if (entry.isIntersecting) {
+    // The footer is in view, remove the "sticky" class
+    document.body.classList.remove("sticky");
+  } else {
+    // The footer is not in view, add the "sticky" class
+    document.body.classList.add("sticky");
+  }
+}, {
   root: null,
   threshold: 0,
-  rootMargin: "-80px",
- })
- obs.observe(sectionHeroEL);
+  rootMargin: "-80px"
+});
+
+stickyObserver.observe(sectionHeroEL);
+stickyObserver.observe(footerEL); // Observe the footer element as well
